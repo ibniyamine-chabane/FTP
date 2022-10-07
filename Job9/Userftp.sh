2959c3947eaa96e30593b6c0f37943288ab96645
ibniyamine-chabane <ibni-yamine.chabane@laplateforme.io>
2022-10-07 15:39:22 +0200

    adding test of Job9

---
+++ Job9/Userftp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#line=Shell_Userlist.csv
+
+cat Shell_Userlist.csv
+
+while read line
+do
+echo "enregistrement est : $line"
+echo ""
+done < Shell_Userlist.csv
+
+while IFS="," read -r col_id col_prenom col_nom col_mdp col_role
+do
+echo "ID: $col_id"
+echo "Prénom: $col_prenom"
+echo "Nom: $col_nom"
+echo "Mdp: $col_mdp"
+echo "Rôle: $col_role"
+echo ""
+done < Shell_Userlist.csv
+
+
